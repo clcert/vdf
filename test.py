2198bab3e4bb412fe557a33ef65a839dd1a3d73d
Bryan <bryan.ortiz@ug.uchile.cl>
2021-11-07 19:54:13 -0300

    Test Time VDF

--- test.py
+++ test.py
@@ -1,53 +1,53 @@
-from chiavdf import (
-    create_discriminant,
-    prove,
-    verify_wesolowski,
-)
+from vdf_wrapper import *
+from statistics import mean, stdev
 from time import perf_counter
-from secrets import token_bytes
+import csv
 
-discriminant_challenge = token_bytes(10)
-discriminant_size = 512
-form_size = 100
-initial_el = b"\x08" + (b"\x00" * 99)
-iters = 1000000
+X_VALUE = b"\x08" + (b"\x00" * 99)
+EXPS = 10
 
-def test_prove_and_verify(
-    discriminant_size, 
-    discriminant_challenge,
-    form_size,
-    initial_el,
-    iterations,
-):
-    
-    discriminant = create_discriminant(discriminant_challenge, discriminant_size)
-    
+def testVDF(lbda, T):
+    pp = setup(lbda,T)
+
+    t0 = perf_counter()
+    y, proof = eval(pp, X_VALUE)
     t1 = perf_counter()
-    result = prove(discriminant_challenge, initial_el, discriminant_size, iterations)
+    is_valid = verify(pp, X_VALUE, y, proof)
     t2 = perf_counter()
 
-    print(f"Prove Time: {(t2 - t1)}")
+    return (t1 - t0), (t2 - t1)
 
-    result_y = result[:form_size]
-    proof = result[form_size : 2 * form_size]
+if __name__ == '__main__':
 
-    t3 = perf_counter()
-    is_valid = verify_wesolowski(
-        str(discriminant),
-        initial_el,
-        result_y,
-        proof,
-        iterations,
-    )
-    t4 = perf_counter()
-    print(f"Verify Time: {(t4-t3)}")
-    assert is_valid
+    csvfile = open('results/exec_time.csv', 'w', newline='')
+    writer = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    
+    for lbda in [256, 512, 1024]:
 
+        writer.writerow([f'Lambda = {lbda}'])
+        writer.writerow([
+            'T',
+            'Eval Mean',
+            'Eval Stdev',
+            'Verify Mean', 
+            'Verify Stdev'
+        ])
 
-test_prove_and_verify(
-    discriminant_size, 
-    discriminant_challenge, 
-    form_size, 
-    initial_el, 
-    iterations
-)
+        for T in range(1,11):
+            T *= 500000
+            tps = []
+            tvs = []
+
+            print(f'λ = {lbda} T = {T}')
+
+            for i in range(EXPS):
+                print(f'Experiment {i+1}/{EXPS}', end='\t')
+                t_prove, t_verify = testVDF(lbda, T)
+                print(f'TP = {t_prove} \t TV = {t_verify}')
+                tps.append(t_prove)
+                tvs.append(t_verify)
+
+            eval_mean, eval_stdev = mean(tps), stdev(tps)
+            verf_mean, verf_stdev = mean(tvs), stdev(tvs)
+
+            writer.writerow([T, eval_mean, eval_stdev, verf_mean, verf_stdev])
