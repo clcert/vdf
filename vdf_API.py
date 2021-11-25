b4a7c6e9abab14d11fad58d7d80388fe17685729
Bryan Ortiz <bryortizp@gmail.com>
2021-11-25 00:37:00 -0300

    Flask API for VDF

---
+++ vdf_API.py
@@ -0,0 +1,75 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from traceback import format_exc
+from secrets import token_bytes
+from chiavdf import (
+    create_discriminant,
+    prove,
+    verify_wesolowski,
+)
+
+app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+CHALLENGE = token_bytes(10)
+FORM_SIZE = 100 # Could be an input
+
+
+@app.route('/create', methods=['POST','GET'])
+@cross_origin()
+def create():
+
+    try:
+        discriminant_size = int(request.form['discriminant_size'])
+        discriminant = create_discriminant(
+            CHALLENGE,
+            discriminant_size
+        )
+        return {'Discriminant': discriminant}
+
+    except Exception as error:
+        return {'Error': str(error)}
+
+
+@app.route('/eval', methods=['GET','POST'])
+@cross_origin()
+def eval():
+    try:
+        x = int(request.form['input']).to_bytes(FORM_SIZE, 'big')
+        T = int(request.form['iterations'])
+        discriminant_size = int(request.form['discriminant_size'])
+
+        result  = prove(CHALLENGE, x, discriminant_size, T)
+        y       = int.from_bytes(result[:FORM_SIZE], 'big')
+        proof   = int.from_bytes(result[FORM_SIZE : 2 * FORM_SIZE], 'big')
+
+        return {'output': y, 'proof': proof}
+
+    except Exception as error:
+        print(format_exc())
+        return {'Error': str(error)}
+
+
+@app.route('/verify', methods=['GET','POST'])
+@cross_origin()
+def verify():
+    try:
+        D   = str(request.form['discriminant'])
+        x   = int(request.form['input']).to_bytes(FORM_SIZE, 'big')
+        y   = int(request.form['output']).to_bytes(FORM_SIZE, 'big')
+        pi  = int(request.form['proof']).to_bytes(FORM_SIZE, 'big')
+        T   = int(request.form['iterations'])
+
+        is_valid = verify_wesolowski(D, x, y, pi, T)
+
+        return {'Valid': is_valid}
+
+    except Exception as error:
+        print(format_exc())
+        return {'Error': str(error)}
+    
+
+if __name__ == '__main__':
+    app.run()
